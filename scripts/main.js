796651e90a17ed8b7f15fdfcd07bf70316b489ff
baijunfeng <baijunfeng@doumi.com>
2017-05-09 18:10:24 +0800

    add json

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Hello world!';
+
+
+
+myHeading.onclick = alertFun
+function alertFun() {
+    alert("JavaScript Alert")
+}
+
+var h2Param = document.querySelector('h2');
+
+h2Param.addEventListener('click', updateContent);
+function updateContent() {
+    var content = prompt('enter a new content');
+    h2Param.textContent = content;
+}
